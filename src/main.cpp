29152bbf5f1e1af91206687967d984b52fb8f894
BlackSound1 <matthewr@videotron.ca>
2024-12-01 18:52:44 -0500

    Added exit builtin function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 bool isValidCommand(const std::string &cmd);
+std::vector<std::string> explodeString(const std::string &cmd);
+int runCommand(const std::string& cmd);
 
 int main() {
 	// Flush after every std::cout / std:cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
-  
+	int exitCode = 0;
 	std::string input;
 
 	while (true) {
@@ -20,17 +23,75 @@ int main() {
 		bool isValid = isValidCommand(input);
 
 		if (isValid) {
-			std::cout << "" << std::endl;
+			exitCode = runCommand(input);
+
+			if (exitCode == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return exitCode;
+			}
 		}
 		else {
 			std::cout << input << ": command not found" << std::endl;
 		}
 	}
 
-   return 0;
+   return exitCode;
+}
+
+int runCommand(const std::string& cmd) {
+	// Get the exploded command
+	auto explodedCommand = explodeString(cmd);
+	std::string cmdName = explodedCommand[0];
+	
+	if (cmdName == "exit")
+	{
+		// If we call exit, must have an exit code
+		if (explodedCommand.size() != 2)
+		{
+			return 1;
+		}
+
+		// Get exit code
+		int exitCode = std::atoi(explodedCommand[1].c_str());
+
+		return exitCode;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 // Checks if a command is valid
 bool isValidCommand(const std::string &cmd) {
-	return false;
+	
+	// Split command into tokens
+	auto explodedCommand = explodeString(cmd);
+
+	// Get the command name
+	std::string cmdName = explodedCommand[0];
+
+	if (cmdName == "exit") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+std::vector<std::string> explodeString(const std::string& cmd)
+{
+	std::vector<std::string> explodedString;
+	std::stringstream ss(cmd);
+
+	std::string token;
+	while (std::getline(ss, token, ' ')) {
+		explodedString.push_back(token);
+	}
+
+	return explodedString;
 }
